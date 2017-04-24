f52b0c850195f6f9304c8bd247faa2198d0bb09f
Ed Stennett <edward.stennett@ackeecreative.com>
2017-04-24 21:28:47 +0100

    Fade in effect added to copy throughout site

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -24,6 +24,29 @@
 
 // Place any jQuery/helper plugins in here.
 
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.fade-out').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
 /* -----------------------------------------------
 /* Author : Vincent Garreau  - vincentgarreau.com
 /* MIT license: http://opensource.org/licenses/MIT
