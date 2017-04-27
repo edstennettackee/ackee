512e7803d33950bf2aa32893963937fc4eec30b0
Ed Stennett <edward.stennett@ackeecreative.com>
2017-04-27 12:48:06 +0100

    Contact.php updated

--- src/contact.php
+++ src/contact.php
@@ -1,21 +1,14 @@
 <?php
-  $to = 'edward.stennett@ackeecreative.com' . "\r\n";
-  $subject = 'Subject';
-  $name = $_POST['name'];
-  $email = $_POST['email']; 
-
-  $message = "************************************************** \r\n" .
-  	         "Message from you website!  \r\n" .
-             "************************************************** \r\n" .	
-    
-  	        "Name: " . $name . "\r\n" .
-  	        "E-mail: " . $email . "\r\n" .
-  	        "Message: " . $_POST["message"] . "\r\n"; 
-
-  $headers = "From: " . $name . "<" . $email . "> \r\n" .
-  	         "Reply-To: " . $email . "\r\n" .
-             "MIME-Version: 1.0" . "\r\n" .
-             "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-	mail($to, $subject, $message, $headers); 
- ?>
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'edward.stennett@ackeecreative.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: edward.stennett@ackeecreative.com\n";
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
+?>
